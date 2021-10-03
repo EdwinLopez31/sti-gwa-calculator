7a5b1ec3bc540a64a1cd2504123960ed9c8c5437
Edwin Lopez <e.lopez103199@gmail.com>
2021-10-03 11:01:11 +0800

    added framer-motion, implemented new  styles, fixed several bugs useStates

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import FocusLock from 'react-focus-lock';
+import FocusLock from 'react-focus-lock'
 
 const Modal = (props) => {
   return (
     <FocusLock>
-      {props.children}
+      <div className="bg-[#f2f2f2] w-1/3 overflow-hidden rounded-md z-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        {props.children}
+      </div>
+
+      <div className="absolute inset-0 z-10 bg-gray-900 bg-opacity-80" />
     </FocusLock>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,241 +4,245 @@ import CustomInput from '../components/CustomInput/CustomInput'
 import GradesContext from '../components/context/GradesContext'
 import Modal from '../components/Modal'
 import * as GradesUtility from '../components/context/GradesUtility'
-
-
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext, useCallback } from 'react'
+import { m, LazyMotion, domAnimation } from 'framer-motion'
 
 const Home = () => {
-	const [inputFields, setInputFields] = useState({
-		name: '',
-		units: '',
-		qt1Grade: '',
-		qt2Grade: '',
-		qt3Grade: '',
-		qt4Grade: '',
-		rawGrade: '',
-		finalGrade: '',
-		creditPointsPerCourse: '',
-	})
+  const [inputFields, setInputFields] = useState({
+    name: '',
+    units: '',
+    qt1Grade: '',
+    qt2Grade: '',
+    qt3Grade: '',
+    qt4Grade: '',
+    rawGrade: '',
+    finalGrade: '',
+    creditPointsPerCourse: '',
+  })
 
-	const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-	const [isClicked, setIsClicked] = useState([false])
-	const {
-		addSubject,
-		calculateGWA,
-		deleteSubject,
-		subjects,
-		GWA,
-		removeAll,
-	} = useContext(GradesContext)
+  const [isClicked, setIsClicked] = useState(false)
+  const { addSubject, calculateGWA, deleteSubject, subjects, GWA, removeAll } =
+    useContext(GradesContext)
 
-	const handleAddSubject = e => {
-		e.preventDefault()
-		inputFields.name &&
-			inputFields.qt1Grade &&
-			inputFields.qt2Grade &&
-			inputFields.qt3Grade &&
-			inputFields.qt4Grade &&
-			setIsClicked(prevState => !prevState)
-		const tempRawGrade = +(
-			+inputFields.qt1Grade * 0.2 +
-			+inputFields.qt2Grade * 0.2 +
-			+inputFields.qt3Grade * 0.2 +
-			+inputFields.qt4Grade * 0.4
-		).toFixed(2)
+  const handleAddSubject = (e) => {
+    e.preventDefault()
 
-		const tempFinGrade = GradesUtility.getEquivalentGrade(tempRawGrade)
+    setIsClicked((prevState) => !prevState)
+    const tempRawGrade = +(
+      +inputFields.qt1Grade * 0.2 +
+      +inputFields.qt2Grade * 0.2 +
+      +inputFields.qt3Grade * 0.2 +
+      +inputFields.qt4Grade * 0.4
+    ).toFixed(2)
 
-		setInputFields({
-			...inputFields,
-			rawGrade: tempRawGrade,
-			finalGrade: tempFinGrade,
-			creditPointsPerCourse: +tempFinGrade * +inputFields.units,
-		})
-	}
+    const tempFinGrade = GradesUtility.getEquivalentGrade(tempRawGrade)
 
-	useEffect(() => {
-		inputFields.creditPointsPerCourse && addSubject({ ...inputFields })
-	}, [isClicked])
+    setInputFields({
+      ...inputFields,
+      rawGrade: tempRawGrade,
+      finalGrade: tempFinGrade,
+      creditPointsPerCourse: +tempFinGrade * +inputFields.units,
+    })
+  }
 
-	const handleChange = e => {
-		const type = e.target.type
-		const value = e.target.value
-		const name = e.target.name
+  useEffect(() => {
+    inputFields.creditPointsPerCourse && addSubject({ ...inputFields })
+  }, [isClicked])
 
-		setInputFields(prevState => ({
-			...prevState,
-			[name]: type === 'number' ? value.length > 0 && parseFloat(value) : value,
-		}))
-	}
+  const handleChange = (e) => {
+    const type = e.target.type
+    const value = e.target.value
+    const name = e.target.name
 
-	return (
-		<div
-			className={`w-full flex flex-col min-h-screen gap-2.5 p-2 text-sm ${styles.mainContainer}`}
-		>
-			<div className="flex flex-col justify-center w-full p-2">
-				<div className="relative mb-4 bg-[#f2f2f2] rounded-md shadow-sm lg:w-[56rem] max-w-4xl lg:mx-auto ">
-					<div className="w-full p-4 mb-10 font-medium text-center text-gray-400 shadow ">
-						<h1 className="uppercase ">Input raw grade</h1>
-						<p className="">(e.g. 95.50, 82.33)</p>
-					</div>
-					<form className="flex flex-col gap-8 p-4">
-						<CustomInput
-							type="text"
-							id="name"
-							name="name"
-							placeholder="Subject Name"
-							value={inputFields.name}
-							onChange={e => handleChange(e)}
-						/>
-						<CustomInput
-							type="number"
-							id="units"
-							name="units"
-							placeholder="Units"
-							value={inputFields.units}
-							onChange={e => handleChange(e)}
-						/>
-						<CustomInput
-							type="number"
-							id="qt1"
-							name="qt1Grade"
-							placeholder="Prelim Grade"
-							value={inputFields.qt1Grade}
-							onChange={e => handleChange(e)}
-						/>
-						<CustomInput
-							type="number"
-							id="qt2"
-							name="qt2Grade"
-							placeholder="Midterm Grade"
-							value={inputFields.qt2Grade}
-							onChange={e => handleChange(e)}
-						/>
-						<CustomInput
-							type="number"
-							id="qt3"
-							name="qt3Grade"
-							placeholder="Prefinal Grade"
-							value={inputFields.qt3Grade}
-							onChange={e => handleChange(e)}
-						/>
-						<CustomInput
-							type="number"
-							id="qt4"
-							name="qt4Grade"
-							placeholder="Finals Grade"
-							value={inputFields.qt4Grade}
-							onChange={e => handleChange(e)}
-						/>
-						<button
-							onClick={e => handleAddSubject(e)}
-							className="px-3 mx-4 rounded-md bg-[#f9cb13] text-white py-1 self-start mb-4 text-sm focus:outline-none"
-						>
-							Add Subject
-						</button>
-					</form>
-				</div>
-			</div>
+    setInputFields((prevState) => ({
+      ...prevState,
+      [name]: type === 'number' ? value.length > 0 && parseFloat(value) : value,
+    }))
+  }
 
-			<div className="container flex flex-col-reverse p-2 mx-auto overflow-x-hidden gap-y-3 lg:w-[56rem] max-w-4xl lg:mx-auto">
-				{subjects.map((subject, idx) => {
-					return (
-						<div
-							initial={{ x: '-100%', opacity: 0 }}
-							animate={{ x: 0, opacity: 1 }}
-							exit={{ x: '100%', opacity: 0 }}
-							transition={{ duration: 0.6 }}
-							key={subject.name + idx}
-							onClick={e => deleteSubject(idx)}
-							className="grid w-full gap-2 md:grid-cols-8 grid-cols-4  shadow bg-opacity-90 rounded-lg text-white bg-[#08416d] gap-y-2.5 cursor-pointer transform duration-300 hover:-translate-y-0.5 hover:shadow-md focus:shadow-md focus:-translate-y-0.5"
-						>
-							<div className="col-span-4 p-2 mb-4 font-bold text-center text-[#f9cb13] uppercase bg-white bg-opacity-[0.15] md:col-span-8 truncate">
-								{subject.name}
-							</div>
-							<div className="flex justify-around w-full col-span-4 px-3 pb-1 md:pb-5 md:pt-0">
-								<div className="text-center">
-									<h2 className="text-xs mb-0.5">Prelims</h2>
-									<p className="">{subject.qt1Grade}</p>
-								</div>
-								<div className="text-center">
-									<h2 className="text-xs mb-0.5">Midterm</h2>
-									<p className="">{subject.qt2Grade}</p>
-								</div>
-								<div className="text-center">
-									<h2 className="text-xs mb-0.5">Pre-finals</h2>
-									<p className="">{subject.qt3Grade}</p>
-								</div>
-								<div className="text-center">
-									<h2 className="text-xs mb-0.5">Finals</h2>
-									<p className="">{subject.qt4Grade}</p>
-								</div>
-							</div>
-							<div className="flex justify-around w-full col-span-4 p-3 text-center md:px-3 md:pb-5 md:pt-0">
-								<div className="">
-									<h2 className="text-xs mb-0.5">Raw Grade</h2>
-									<p className="">{subject.rawGrade}</p>
-								</div>
-								<div className="">
-									<h2 className="text-xs mb-0.5">Subject Grade</h2>
-									<p className="font-bold">{subject.finalGrade}</p>
-								</div>
-							</div>
-						</div>
-					)
-				})}
-			</div>
+  return (
+    <div
+      className={`w-full grid xl:grid-cols-3 grid-cols-1 min-h-screen text-sm p-4 gap-x-4 ${styles.mainContainer}`}
+    >
+      <Head>
+        <title>STI GWA Calculator</title>
+      </Head>
 
-			{subjects.length > 0 && (
-				<div className="flex flex-col justify-end lg:w-[56rem] max-w-4xl lg:mx-auto col-start-2 gap-4 p-6">
-					<button
-						onClick={e => {
-							calculateGWA(), setIsModalOpen(prevState => !prevState)
-						}}
-						className="px-2 rounded-md bg-[#08416d] self-end py-1 font-semibold text-sm focus:outline-none transition duration-300 shadow text-white hover:text-white hover:bg-[#063456]"
-					>
-						Calculate GWA
-					</button>
-					<button
-						onClick={removeAll}
-						className="px-2 rounded-md border border-[#e51d1d] self-end font-semibold py-1 text-sm focus:outline-none hover:bg-[#e51d1d] hover:text-white transition duration-300 shadow text-[#e51d1d]"
-					>
-						Remove Subjects
-					</button>
-					<Modal>
-						<div
-							className={`bg-opacity-40 fixed inset-0 items-center justify-center bg-black ${isModalOpen ? 'flex' : 'hidden'
-								}`}
-						>
-							<div className="bg-[#f2f2f2] w-1/3 p-4 rounded-md">
-								<header className="inset-x-0 top-0 flex justify-end">
-									<button
-										onClick={e => setIsModalOpen(false)}
-										className="focus:outline-none group"
-									>
-										<svg
-											className="w-5 h-5 text-black fill-current group-hover:text-gray-700"
-											viewBox="0 0 20 20"
-										>
-											<path
-												fillRule="evenodd"
-												d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-												clipRule="evenodd"
-											/>
-										</svg>
-									</button>
-								</header>
-								<main className="flex items-center justify-center font-bold">
-									Your GWA is: {GWA}
-								</main>
-							</div>
-						</div>
-					</Modal>
-				</div>
-			)}
-		</div>
-	)
+      <div className="flex flex-col w-full col-span-1 p-2">
+        <div className="relative mb-4 lg:mb-0 bg-[#f2f2f2] bg-opacity-90 backdrop-filter backdrop-blur-sm rounded-md shadow-sm lg:w-full lg:mx-auto transition-[height] duration-200">
+          <div className="w-full p-4 mb-10 font-medium text-center text-gray-500 shadow ">
+            <h1 className="uppercase ">Input raw grade</h1>
+            <p className="">(e.g. 95.50, 82.33)</p>
+            <h1
+              className={`${
+                subjects.length > 0 ? 'opacity-100' : 'opacity-0'
+              } transition-opacity mt-2 uppercase font-bold `}
+            >
+              Click on a subject to remove it from the list
+            </h1>
+          </div>
+          <form
+            onSubmit={(e) => handleAddSubject(e)}
+            className="flex flex-col items-center gap-8 p-4"
+          >
+            <CustomInput
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Subject Name"
+              value={inputFields.name}
+              onChange={(e) => handleChange(e)}
+            />
+            <CustomInput
+              type="number"
+              id="units"
+              name="units"
+              placeholder="Units"
+              value={inputFields.units}
+              onChange={(e) => handleChange(e)}
+            />
+            <CustomInput
+              type="number"
+              id="qt1"
+              name="qt1Grade"
+              placeholder="Prelim Grade"
+              value={inputFields.qt1Grade}
+              onChange={(e) => handleChange(e)}
+            />
+            <CustomInput
+              type="number"
+              id="qt2"
+              name="qt2Grade"
+              placeholder="Midterm Grade"
+              value={inputFields.qt2Grade}
+              onChange={(e) => handleChange(e)}
+            />
+            <CustomInput
+              type="number"
+              id="qt3"
+              name="qt3Grade"
+              placeholder="Prefinal Grade"
+              value={inputFields.qt3Grade}
+              onChange={(e) => handleChange(e)}
+            />
+            <CustomInput
+              type="number"
+              id="qt4"
+              name="qt4Grade"
+              placeholder="Finals Grade"
+              value={inputFields.qt4Grade}
+              onChange={(e) => handleChange(e)}
+            />
+            <div className="flex flex-col items-center justify-center w-full gap-y-4 md:flex-row gap-x-4">
+              {subjects.length > 0 && (
+                <button
+                  onClick={(e) => {
+                    e.preventDefault()
+                    calculateGWA()
+                    setIsModalOpen((prevState) => !prevState)
+                  }}
+                  className="px-3 md:px-4 py-2 rounded-md bg-[#08416d] font-semibold text-sm focus:outline-none transition duration-300 shadow text-white hover:text-white hover:bg-[#052c4b]"
+                >
+                  Calculate GWA
+                </button>
+              )}
+              <button className="px-3 md:px-4 rounded-md bg-[#e9c01c] text-white py-2  text-sm focus:outline-none hover:shadow-sm transition-transform hover:-translate-y-1">
+                Add Subject
+              </button>
+              {subjects.length > 0 && (
+                <button
+                  onClick={(e) => {
+                    e.preventDefault()
+                    removeAll()
+                  }}
+                  className="px-3 md:px-4 py-2 rounded-md ring-2 ring-[#e51d1d] ring-inset font-semibold text-sm focus:outline-none hover:bg-[#e51d1d] hover:text-white transition duration-300 shadow text-[#e51d1d]"
+                >
+                  Remove All Subjects
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="grid w-full grid-cols-1 col-span-2 grid-rows-6 p-2 overflow-x-hidden gap-x-3 lg:grid-cols-2 gap-y-3 lg:w-full">
+        <LazyMotion features={domAnimation}>
+          {subjects.map((subject, idx) => {
+            return (
+              <m.div
+                initial={{ x: '100%', opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.1 }}
+                key={subject.name + idx}
+                onClick={(e) => deleteSubject(idx)}
+                className="grid w-full gap-2 md:h-32 h-44 md:grid-cols-8 grid-cols-4 overflow-hidden shadow  ring-white ring-[2px]  rounded-lg text-white bg-[#08416d] gap-y-2.5 cursor-pointer transform duration-300 hover:-translate-y-0.5 hover:shadow-md focus:shadow-md focus:-translate-y-0.5"
+              >
+                <div className="col-span-4 p-2 mb-4 font-bold text-center text-[#f9cb13] uppercase bg-white bg-opacity-[0.15] md:col-span-8 truncate">
+                  {subject.name}
+                </div>
+                <div className="flex justify-around w-full col-span-4 pb-1 md:pb-5 md:pt-0">
+                  <div className="text-center">
+                    <h2 className="text-xs mb-0.5">Prelims</h2>
+                    <p className="">{subject.qt1Grade}</p>
+                  </div>
+                  <div className="text-center">
+                    <h2 className="text-xs mb-0.5">Midterm</h2>
+                    <p className="">{subject.qt2Grade}</p>
+                  </div>
+                  <div className="text-center">
+                    <h2 className="text-xs mb-0.5">Pre-finals</h2>
+                    <p className="">{subject.qt3Grade}</p>
+                  </div>
+                  <div className="text-center">
+                    <h2 className="text-xs mb-0.5">Finals</h2>
+                    <p className="">{subject.qt4Grade}</p>
+                  </div>
+                </div>
+                <div className="flex justify-around w-full col-span-4 p-3 text-center md:px-3 md:pb-5 md:pt-0">
+                  <div className="">
+                    <h2 className="text-xs mb-0.5">Raw Grade</h2>
+                    <p className="">{subject.rawGrade}</p>
+                  </div>
+                  <div className="">
+                    <h2 className="text-xs mb-0.5">Subject Grade</h2>
+                    <p className="font-bold">{subject.finalGrade}</p>
+                  </div>
+                </div>
+              </m.div>
+            )
+          })}
+        </LazyMotion>
+      </div>
+
+      {isModalOpen && (
+        <Modal>
+          <header className="flex justify-end w-full h-full p-4 mb-4 bg-gray-300">
+            <button
+              onClick={(e) => setIsModalOpen(false)}
+              className="focus:outline-none group"
+            >
+              <svg
+                className="w-5 h-5 text-black fill-current group-hover:text-gray-700"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </header>
+          <main className="flex flex-col items-center justify-center p-4 font-bold ">
+            <h1 className="uppercase"> Your GWA is: </h1>
+            <p className="text-2xl md:text-3xl">{GWA}</p>
+          </main>
+        </Modal>
+      )}
+    </div>
+  )
 }
 
 export default Home
