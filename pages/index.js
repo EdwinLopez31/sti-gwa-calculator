0f14bdce224b035e47ac5981a8fefeebeb5c4217
Edwin Lopez <e.lopez103199@gmail.com>
2021-10-03 12:15:06 +0800

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -62,10 +62,29 @@ const Home = () => {
   }
 
   return (
-    <div
-      className={`${styles.mainContainer}`}
-    >
+    <div className={`${styles.mainContainer}`}>
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <title>STI GWA Calculator</title>
       </Head>
       <div className="grid w-full min-h-screen grid-cols-1 p-4 text-sm xl:grid-cols-3 gap-x-4">
